5abc1df139385a4780f283ed1856c96c18d63915
jinliang.cao <jinliang.cao@phicomm.com>
2018-09-11 16:32:39 +0800

    modify WX red packet pick actions

--- app/src/main/java/com/example/cao_j/redpaketautopick/RedPacketPickService.java
+++ app/src/main/java/com/example/cao_j/redpaketautopick/RedPacketPickService.java
@@ -1,10 +1,14 @@
 package com.example.cao_j.redpaketautopick;
 
 import android.accessibilityservice.AccessibilityService;
+import android.app.KeyguardManager;
 import android.app.Notification;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.PowerManager;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityEventSource;
@@ -15,126 +19,256 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RedPacketPickService extends AccessibilityService {
-    public RedPacketPickService() {
-    }
+    /**
+     * 微信几个页面的包名+地址。用于判断在哪个页面
+     * LAUCHER-微信聊天界面
+     * LUCKEY_MONEY_RECEIVER-点击红包弹出的界面
+     * LUCKEY_MONEY_DETAIL-红包领取后的详情界面
+     */
+    private String LAUCHER = "com.tencent.mm.ui.LauncherUI";
+    private String LUCKEY_MONEY_DETAIL = "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI";
+    private String LUCKEY_MONEY_RECEIVER = "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI";
 
     /**
-     * 有关AccessibilityEvent事件的回调函数，
-     * 系统通过sendAccessibiliyEvent()不断的发送AccessibilityEvent到此处
+     * 用于判断是否点击过红包了
      */
-    @Override
-    public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
-        int eventType = accessibilityEvent.getEventType();
-        AccessibilityNodeInfo nodeInfo = accessibilityEvent.getSource();
-        String className = accessibilityEvent.getClassName().toString();
+    private boolean isOpenRP;
 
+    private boolean isOpenDetail = false;
+
+    /**
+     * 用于判断是否屏幕是亮着的
+     */
+    private boolean isScreenOn;
+
+    /**
+     * 获取PowerManager.WakeLock对象
+     */
+    private PowerManager.WakeLock wakeLock;
+
+    /**
+     * KeyguardManager.KeyguardLock对象
+     */
+    private KeyguardManager.KeyguardLock keyguardLock;
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        int eventType = event.getEventType();
         Log.d("caojinliang","拦截事件 eventType = " + eventType);
-//        Log.d("caojinliang","拦截事件 nodeInfo = " + nodeInfo.toString());
-        Log.d("caojinliang","拦截事件 className = " + className);
-        switch (eventType){
+        Log.d("caojinliang","拦截事件 className = " + event.getClassName().toString());
+        Log.d("caojinliang","拦截事件 getSource = " + event.getSource().toString());
+        switch (eventType) {
+            //通知栏来信息，判断是否含有微信红包字样，是的话跳转
             case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
-                List<CharSequence> texts = accessibilityEvent.getText();
-                if(!texts.isEmpty()){
-                    for(CharSequence text : texts){
-                        String content = text.toString();
-                        if(content.contains("[微信红包]")){
-                            dealWithWXNotification(accessibilityEvent);
+                List<CharSequence> texts = event.getText();
+                for (CharSequence text : texts) {
+                    String content = text.toString();
+                    if (!TextUtils.isEmpty(content)) {
+                        //判断是否含有[微信红包]字样
+                        if (content.contains("[微信红包]")) {
+                            if (!isScreenOn()) {
+                                wakeUpScreen();
+                            }
+                            //如果有则打开微信红包页面
+                            openWeChatPage(event);
+
+                            isOpenRP = false;
                         }
                     }
                 }
                 break;
+            //界面跳转的监听
             case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
-                if(className.equals("com.tencent.mm.ui.LauncherUI")){
-                    Log.d("caojinliang","点击红包");
-                    getLastPacket();
-                }else if(className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI")){
-                    Log.d("caojinliang","开红包");
-                    inputClick("com.tencent.mm:id/bg7");
-                }else if(className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI")){
-                    Log.d("caojinliang","退出红包");
-                    inputClick("com.tencent.mm:id/gd");
+                String className = event.getClassName().toString();
+                //判断是否是微信聊天界面
+                if (LAUCHER.equals(className)) {
+                    //获取当前聊天页面的根布局
+                    AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+                    //开始找红包
+                    findRedPacket(rootNode);
+                }
+
+                //判断是否是显示‘开’的那个红包界面
+                if (LUCKEY_MONEY_RECEIVER.equals(className)) {
+                    Log.d("caojinliang", "开红包界面：node = " + event.getSource().toString());
+                    AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+                    //开始抢红包
+                    if(rootNode != null){
+                        openRedPacket(rootNode);
+                    }else if(event.getSource() != null){
+                        openRedPacket(event.getSource());
+                    }
+
+                }
+
+                //判断是否是红包领取后的详情界面
+                if (isOpenDetail && LUCKEY_MONEY_DETAIL.equals(className)) {
+
+                    isOpenDetail = false;
+                    //返回桌面
+//                    back2Home();
+                    //如果之前是锁着屏幕的则重新锁回去
+                    release();
                 }
                 break;
+                default:
+        }
+
+
+    }
+
+    /**
+     * 开始打开红包
+     */
+    private void openRedPacket(AccessibilityNodeInfo rootNode) {
+        for (int i = 0; i < rootNode.getChildCount(); i++) {
+            AccessibilityNodeInfo node = rootNode.getChild(i);
+            if ("android.widget.Button".equals(node.getClassName())) {
+                node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+
+                isOpenDetail = true;
+            }
+            openRedPacket(node);
         }
     }
 
-    private void dealWithWXNotification(AccessibilityEvent event) {
-        if(event.getParcelableData() != null &&
-                event.getParcelableData() instanceof Notification){
-            Notification notification = (Notification)event.getParcelableData();
+    /**
+     * 遍历查找红包
+     */
+    private void findRedPacket(AccessibilityNodeInfo rootNode) {
+        if (rootNode != null) {
+            //从最后一行开始找起
+            for (int i = rootNode.getChildCount() - 1; i >= 0; i--) {
+                AccessibilityNodeInfo node = rootNode.getChild(i);
+                //如果node为空则跳过该节点
+                if (node == null) {
+                    continue;
+                }
+                CharSequence text = node.getText();
+                if (text != null && text.toString().equals("领取红包")) {
+                    AccessibilityNodeInfo parent = node.getParent();
+                    //while循环,遍历"领取红包"的各个父布局，直至找到可点击的为止
+                    while (parent != null) {
+                        if (parent.isClickable()) {
+                            //模拟点击
+                            parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                            //isOpenRP用于判断该红包是否点击过
+                            isOpenRP = true;
+
+                            break;
+                        }
+                        parent = parent.getParent();
+                    }
+                }
+                //判断是否已经打开过那个最新的红包了，是的话就跳出for循环，不是的话继续遍历
+                if (isOpenRP) {
+                    break;
+                } else {
+                    findRedPacket(node);
+                }
+
+            }
+        }
+    }
+
+    /**
+     * 开启红包所在的聊天页面
+     */
+    private void openWeChatPage(AccessibilityEvent event) {
+        //A instanceof B 用来判断内存中实际对象A是不是B类型，常用于强制转换前的判断
+        if (event.getParcelableData() != null && event.getParcelableData() instanceof Notification) {
+            Notification notification = (Notification) event.getParcelableData();
+            //打开对应的聊天界面
             PendingIntent pendingIntent = notification.contentIntent;
-            try{
+            try {
                 pendingIntent.send();
-                Log.d("caojinliang", "打开微信");
-            }catch (Exception e){
+            } catch (PendingIntent.CanceledException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private void inputClick(String clickId){
-        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
-        if(nodeInfo != null){
-            List<AccessibilityNodeInfo> list =
-                    nodeInfo.findAccessibilityNodeInfosByViewId(clickId);
-            for(AccessibilityNodeInfo item : list){
-                item.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-            }
-        }
-    }
 
-    private List<AccessibilityNodeInfo> mParents = new ArrayList<>();
-    private void getLastPacket(){
-        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
-        mParents.clear();
-        recycle(rootNode);
-        if(mParents.size()>0){
-            mParents.get(mParents.size() - 1).performAction(AccessibilityNodeInfo.ACTION_CLICK);
-        }
-    }
-
-    //回归函数遍历每一个节点，并将“领取红包”存进list中
-    public void recycle(AccessibilityNodeInfo info){
-        if(info.getChildCount() == 0){
-            if("领取红包".equals(info.getText().toString())){
-                if(info.isClickable()){
-                    info.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                }
-                AccessibilityNodeInfo parent = info.getParent();
-                while(parent != null){
-                    if(parent.isClickable()){
-                        mParents.add(parent);
-                        break;
-                    }
-                    parent = parent.getParent();
-                }
-            }else{
-                for(int i = 0; i < info.getChildCount(); i++){
-                    if(info.getChild(i) != null){
-                        recycle(info.getChild(i));
-                    }
-                }
-            }
-        }
-    }
-
-    @Override
-    public void onInterrupt() {
-
-    }
-
+    /**
+     * 服务连接
+     */
     @Override
     protected void onServiceConnected() {
+        Toast.makeText(this, "抢红包服务开启", Toast.LENGTH_SHORT).show();
         super.onServiceConnected();
-        Toast.makeText(this, "抢红包服务开启",
-                Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * 必须重写的方法：系统要中断此service返回的响应时会调用。在整个生命周期会被调用多次。
+     */
+    @Override
+    public void onInterrupt() {
+        Toast.makeText(this, "我快被终结了啊-----", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 服务断开
+     */
     @Override
     public boolean onUnbind(Intent intent) {
-        Toast.makeText(this, "抢红包服务关闭",
-                Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "抢红包服务已被关闭", Toast.LENGTH_SHORT).show();
         return super.onUnbind(intent);
     }
 
+    /**
+     * 返回桌面
+     */
+    private void back2Home() {
+        Intent home = new Intent(Intent.ACTION_MAIN);
+        home.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        home.addCategory(Intent.CATEGORY_HOME);
+        startActivity(home);
+    }
+
+    /**
+     * 判断是否处于亮屏状态
+     *
+     * @return true-亮屏，false-暗屏
+     */
+    private boolean isScreenOn() {
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        isScreenOn = pm.isScreenOn();
+        Log.e("isScreenOn", isScreenOn + "");
+        return isScreenOn;
+    }
+
+    /**
+     * 解锁屏幕
+     */
+    private void wakeUpScreen() {
+
+        //获取电源管理器对象
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        //后面的参数|表示同时传入两个值，最后的是调试用的Tag
+        wakeLock = pm.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.FULL_WAKE_LOCK, "bright");
+
+        //点亮屏幕
+        wakeLock.acquire();
+
+        //得到键盘锁管理器
+        KeyguardManager km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        keyguardLock = km.newKeyguardLock("unlock");
+
+        //解锁
+        keyguardLock.disableKeyguard();
+    }
+
+    /**
+     * 释放keyguardLock和wakeLock
+     */
+    public void release() {
+        if (keyguardLock != null) {
+            keyguardLock.reenableKeyguard();
+            keyguardLock = null;
+        }
+        if (wakeLock != null) {
+            wakeLock.release();
+            wakeLock = null;
+        }
+    }
 }
